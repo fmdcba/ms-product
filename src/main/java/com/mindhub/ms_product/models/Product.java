84406eb34d16d4b1873bdfe63b3e09d001382078
fmdcba <facudiaz1738@hotmail.com>
2025-01-21 23:05:40 -0300

    add model

---
+++ src/main/java/com/mindhub/ms_product/models/Product.java
@@ -0,0 +1,64 @@
+package com.mindhub.ms_product.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name, description;
+
+    private Double price;
+
+    private Integer stock;
+
+    public Product(String name, String description, Double price, Integer stock) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+}
