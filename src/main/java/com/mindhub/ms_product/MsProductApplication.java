8b49c8fccc833305366be9eb34db59d54fe43702
fmdcba <facudiaz1738@hotmail.com>
2025-01-21 23:05:28 -0300

    add data for api test

--- src/main/java/com/mindhub/ms_product/MsProductApplication.java
+++ src/main/java/com/mindhub/ms_product/MsProductApplication.java
@@ -1,7 +1,11 @@
 package com.mindhub.ms_product;
 
+import com.mindhub.ms_product.Repositories.ProductRepository;
+import com.mindhub.ms_product.models.Product;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MsProductApplication {
@@ -10,4 +14,14 @@ public class MsProductApplication {
 		SpringApplication.run(MsProductApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initData(ProductRepository productRepository) {
+		return args -> {
+			Product product = new Product("Martillo", "Cabo de madera", 10.5, 17);
+			Product product1 = new Product("Tornillo", "Para madera", 5.1, 199);
+
+			productRepository.save(product);
+			productRepository.save(product1);
+		};
+	}
 }
